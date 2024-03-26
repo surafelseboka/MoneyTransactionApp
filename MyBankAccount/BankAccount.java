0f09991e5a89fcf11416ed7a31d7b3416deaba08
surafelseboka <surafelmars20@gmail.com>
2024-03-26 13:40:49 -0700

    Added functionality on overDraft and able to modify it

--- MyBankAccount/BankAccount.java
+++ MyBankAccount/BankAccount.java
@@ -23,25 +23,7 @@ public class BankAccount {
 
         return balance;
     }
-//    public void transferFunds(Checking checking, Saving saving, double amount){
-//
-//        if (checking.getBalance() >= amount){
-//            checking.balance -= amount;
-//            saving.balance += amount;
-//            System.out.println("Successfully transferred $" + amount + " from " +
-//                    checking.getClass().getSimpleName() + " to " +
-//                    saving.getClass().getSimpleName());
-//        } else if (checking.getBalance() + checking.getOverdraft_limit() >= amount){
-//           checking.balance -= amount;
-//           saving.balance += amount;
-//            System.out.println("Successfully transferred $" + amount + " from " +
-//                    checking.getClass().getSimpleName() + " to " +
-//                    saving.getClass().getSimpleName() + ". Overdraft used.");
-//        } else {
-//            System.out.println("Insufficient funds for transfer.");
-//        }
-//
-//    }
+
 
     public void transferFunds(Checking checking, Saving saving, double amount){
         if (checking.withdraw(amount)){
