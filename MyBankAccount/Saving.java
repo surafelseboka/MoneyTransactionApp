afb60360afd9f3bba65d81db5294e531af6e001c
surafelseboka <surafelmars20@gmail.com>
2024-03-26 14:02:51 -0700

    updated withdraw method

--- MyBankAccount/Saving.java
+++ MyBankAccount/Saving.java
@@ -15,26 +15,22 @@ public class Saving extends BankAccount {
         balance += amount;
     }
 
-    public double applyOverDraftFee(double amount){
-        double overDraft = amount - balance - overdraft_limit;
-        if (overDraft > 0){
-            double fee = overDraft * 0.15;
-            balance = fee * -1;
-        } else{
-            balance = overDraft;
-        }
-        return balance;
-    }
-
     @Override
-    public boolean withdraw(double amount) {
-        if (amount > balance) {
-            balance = applyOverDraftFee(amount);
-            return true;
-        } else {
-            balance -= amount;
+    public boolean withdraw(double amount){
+        if (amount > balance + overdraft_limit){
+            System.out.println("Insufficient funds for withdrawal, including overdraft.");
             return false;
         }
+
+        //Apply overdraft if needed
+        if (amount > balance){
+            double overDraftNeeded = amount - balance;
+            balance -= amount; // Balance now goes to negative
+            System.out.println("Overdraft used. New balance: " + balance);
+        } else{
+            balance -= amount;
+        }
+        return true;
     }
 
     @Override
